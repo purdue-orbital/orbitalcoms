9d6d3e60819f6c4fad63726df0a370282accedad
MattToast <matthew.drozt@gmail.com>
2021-12-31 04:26:00 -0500

    add test to make sure classes can be reached from top level import

---
+++ tests/test_imports.py
@@ -0,0 +1,18 @@
+def test_top_level_imports():
+    """Test important importorts can be accessaed from top level"""
+    from orbitalcoms import (  # noqa: F401
+        BaseComsDriver,
+        ComsMessage,
+        ComsSubscription,
+        GroundStation,
+        LaunchStation,
+        LocalComsDriver,
+        OneTimeComsSubscription,
+        SerialComsDriver,
+        SocketComsDriver,
+        construct_message,
+        create_serial_ground_station,
+        create_serial_launch_station,
+        create_socket_ground_station,
+        create_socket_luanch_station,
+    )
