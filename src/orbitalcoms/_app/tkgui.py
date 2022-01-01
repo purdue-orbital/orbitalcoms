6296b9222e53792fff4515166f0a215204558fb7
MattToast <matthew.drozt@gmail.com>
2021-12-31 20:35:17 -0500

    Add frames for displaying state and data

--- src/orbitalcoms/_app/tkgui.py
+++ src/orbitalcoms/_app/tkgui.py
@@ -1,10 +1,13 @@
-"""Ugly and simple gui for testing and debug
-purposes using tkinter
+"""Ugly, inefficent, and all round grabage gui in tkinter
+for testing, mocking, and debuging purposes
 """
 from __future__ import annotations
 
+import json
 import tkinter as tk
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Optional
+
+from orbitalcoms.coms.messages.message import ComsMessage
 
 if TYPE_CHECKING:
     from ..stations.groundstation import GroundStation
@@ -18,19 +21,37 @@ class GroundStationFrame(tk.Frame):
         self.master.columnconfigure(index=0, weight=1)
         self.master.rowconfigure(index=0, weight=1)
 
-        self.columnconfigure(index=0, weight=1)
+        self.columnconfigure(
+            index=0,
+            weight=1,
+            uniform="1",
+        )
         self.rowconfigure(index=0, weight=1)
         self.rowconfigure(index=1, weight=1)
         self.rowconfigure(index=2, weight=1)
         self.rowconfigure(index=3, weight=1)
         self.rowconfigure(index=4, weight=1)
 
+        self.columnconfigure(index=1, weight=1, uniform="1")
+        self.columnconfigure(index=2, weight=1, uniform="1")
+        self.columnconfigure(index=3, weight=1, uniform="1")
+
         # Buttons
-        self.btn_arm = tk.Button(self, text="ARM")
-        self.btn_abort = tk.Button(self, text="ABORT")
-        self.btn_qdm = tk.Button(self, text="QDM")
-        self.btn_stab = tk.Button(self, text="STABALIZE")
-        self.btn_launch = tk.Button(self, text="LAUNCH")
+        self.btn_arm = tk.Button(
+            self, text="ARM", command=lambda: self.send_state("ARMED")
+        )
+        self.btn_abort = tk.Button(
+            self, text="ABORT", command=lambda: self.send_state("ABORT")
+        )
+        self.btn_qdm = tk.Button(
+            self, text="QDM", command=lambda: self.send_state("QDM")
+        )
+        self.btn_stab = tk.Button(
+            self, text="STABALIZE", command=lambda: self.send_state("STAB")
+        )
+        self.btn_launch = tk.Button(
+            self, text="LAUNCH", command=lambda: self.send_state("LAUNCH")
+        )
 
         self.btn_arm.grid(column=0, row=0, sticky="nsew")
         self.btn_abort.grid(column=0, row=1, sticky="nsew")
@@ -38,13 +59,60 @@ class GroundStationFrame(tk.Frame):
         self.btn_stab.grid(column=0, row=3, sticky="nsew")
         self.btn_launch.grid(column=0, row=4, sticky="nsew")
 
-        # Message Frames
+        # Text
+        self.txt_sent = tk.Text(self)
+        self.txt_recv = tk.Text(self)
+        self.txt_data = tk.Text(self)
 
-        # Data Frame
+        self.txt_sent.grid(column=1, row=0, rowspan=5, sticky="nsew")
+        self.txt_recv.grid(column=2, row=0, rowspan=5, sticky="nsew")
+        self.txt_data.grid(column=3, row=0, rowspan=5, sticky="nsew")
+
+        self.update_disp()
+
+    def send_state(self, update: str) -> None:
+        state = (
+            self._gs.last_sent.as_dict
+            if self._gs.last_sent
+            else ComsMessage(0, 0, 0, 0).as_dict
+        )
+        state[update] = 0 if state[update] else 1
+        self._gs.send(state)
+        self.update_disp()
+
+    def update_disp(self) -> None:
+        def coms_msg_txt_fomat(msg: Optional[ComsMessage], title: str = "") -> str:
+            if title:
+                title = f"{title}:\n" + "=" * len(title) + "\n"
+            if msg is None:
+                return title + "None!"
+            return title + (
+                f"ARMED:  {msg.ARMED}\n"
+                f"ABORT:  {msg.ABORT}\n"
+                f"QDM:    {msg.QDM}\n"
+                f"STAB:   {msg.STAB}\n"
+                f"LAUNCH: {msg.LAUNCH}\n"
+            )
+
+        self.txt_sent.delete(1.0, "end")
+        self.txt_sent.insert(1.0, coms_msg_txt_fomat(self._gs.last_sent, "SENT"))
+
+        self.txt_recv.delete(1.0, "end")
+        self.txt_recv.insert(1.0, coms_msg_txt_fomat(self._gs.last_recieved, "RECV"))
+
+        self.txt_data.delete(1.0, "end")
+        try:
+            data_str = json.dumps(self._gs.data, indent=2)
+        except Exception:
+            data_str = str(self._gs.data)
+        self.txt_data.insert(1.0, "DATA:\n====\n" + data_str)
 
 
 def run_app(gs: GroundStation) -> None:
     root = tk.Tk()
+    width = 800
+    height = 600
+    root.geometry(f"{width}x{height}")
     gs_gui = GroundStationFrame(gs, master=root)
     gs_gui.mainloop()
 
@@ -52,5 +120,9 @@ def run_app(gs: GroundStation) -> None:
 if __name__ == "__main__":
     from ..coms.drivers.localcomsdriver import LocalComsDriver
     from ..stations.groundstation import GroundStation  # noqa: F811, will be removed
+    from ..stations.stationcreators import (  # noqa: F401, will be removed
+        create_socket_ground_station,
+    )
 
     run_app(GroundStation(LocalComsDriver()))
+    # run_app(create_socket_ground_station())
