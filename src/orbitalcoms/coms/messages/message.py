cde606de266980237941537f981c5112d820d99f
Chris Page <page83@purdue.edu>
2022-01-31 20:51:56 -0500

    added tests, checked types correctly

--- src/orbitalcoms/coms/messages/message.py
+++ src/orbitalcoms/coms/messages/message.py
@@ -5,7 +5,7 @@ from xmlrpc.client import Boolean
 from attrs import define, frozen, asdict, validators, field
 import json
 from dataclasses import dataclass
-from typing import Any, Dict, Union
+from typing import Any, Dict, Type, Union
 
 from ..errors.errors import ComsMessageParseError
 
@@ -19,31 +19,50 @@ class ComsMessage:
     QDM: int = field()
     STAB: int = field()
     LAUNCH: int = field()
-    ARMED: int | None = None
-    DATA: Dict[str, Any] | None = None
+    ARMED: int | None = field(default =  None)
+    DATA: Dict[str, Any] | None = field(default = None)
     
-    # TODO: ARMED and DATA are not validated.
-    # TODO: I don't think any tests currently exist to test the following validators.
+    # TODO: resolve booleans to int, rather than simply accepting them
 
     @ABORT.validator
-    def check(self, attribute, value):
+    def check_abort(self, attribute: str, value: Any) -> None:
         if not (type(self.ABORT) == Boolean or type(self.ABORT) == int):
             raise TypeError("ABORT must be either a Boolean or an int")
+        #if (type(self.ABORT) == Boolean):
+        #    self.ABORT = 1
     
     @QDM.validator
-    def check(self, attribute, value):
+    def check_qdm(self, attribute: str, value: Any) -> None:
         if not (type(self.QDM) == Boolean or type(self.QDM) == int):
             raise TypeError("QDM must be either a Boolean or an int")
+        #if (type(self.QDM) == Boolean):
+        #    self.QDM = 1
     
     @STAB.validator
-    def check(self, attribute, value):
+    def check_stab(self, attribute: str, value: Any) -> None:
         if not (type(self.STAB) == Boolean or type(self.STAB) == int):
             raise TypeError("STAB must be either a Boolean or an int")
+        #if (type(self.STAB) == Boolean):
+        #    self.STAB = 1
     
     @LAUNCH.validator
-    def check(self, attribute, value):
+    def check_launch(self, attribute: str, value: Any) -> None:
         if not (type(self.LAUNCH) == Boolean or type(self.LAUNCH) == int):
             raise TypeError("LAUNCH must be either a Boolean or an int")
+        #if (type(self.LAUNCH) == Boolean):
+        #    self.LAUNCH = 1
+    
+    @ARMED.validator
+    def check_armed(self, attribute: str, value: Any) -> None:
+        if not (type(self.ARMED) == Boolean or type(self.ARMED) == int or self.ARMED == None):
+            raise TypeError("ARMED must be either a Boolean, an int, or None")
+        #if (type(self.ARMED) == Boolean):
+        #    self.ARMED = 1
+    
+    @DATA.validator
+    def check_data(self, attribute: str, value: Any) -> None:
+        if not (type(self.DATA) == dict or self.DATA == None):
+            raise TypeError("DATA must be either a Dict or None")
     
     @classmethod
     def from_string(cls, s: str) -> ComsMessage:
@@ -73,6 +92,8 @@ def construct_message(m: ParsableComType) -> ComsMessage:
             return ComsMessage.from_string(m)
         if isinstance(m, dict):
             return ComsMessage.from_string(json.dumps(m))
+    except TypeError:
+        raise
     except Exception as e:
         raise ComsMessageParseError(f"Failed to parse ComsMessage from {m}") from e
     raise TypeError(f"Cannot construct a Coms message from type: {type(m)}")
