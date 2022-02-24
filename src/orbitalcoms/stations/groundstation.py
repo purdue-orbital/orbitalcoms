c96a3a0a73405425537430a1e14bc837e96b2a6b
Chris Page <page83@purdue.edu>
2022-02-23 19:14:44 -0500

    i suck at ctrl+s

--- src/orbitalcoms/stations/groundstation.py
+++ src/orbitalcoms/stations/groundstation.py
@@ -67,10 +67,10 @@ class GroundStation(Station):
             if not self._is_valid_state_change(message):
                 return False
             
-            if self._timeout_thread.is_alive():
-                self._timeout_thread.stop()
-            self._timeout_thread = self.StoppableThread(self.resend_last)
-            self._timeout_thread.start()
+            # if self._timeout_thread.is_alive():
+            #     self._timeout_thread.stop()
+            # self._timeout_thread = self.StoppableThread(self.resend_last)
+            # self._timeout_thread.start()
         except Exception: 
             return False
 
