45c3f21eab0c72a8e4e97a2668aa9177bf9ac8e8
Chris Page <page83@purdue.edu>
2022-03-21 20:24:21 -0400

    added property methods

--- src/orbitalcoms/stations/station.py
+++ src/orbitalcoms/stations/station.py
@@ -104,6 +104,14 @@ class Station(ABC):
     @property
     def last_received(self) -> ComsMessage | None:
         return self._last_received
+    
+    @property
+    def last_sent_time(self) -> float | None:
+        return self._last_sent_time
+    
+    @property
+    def last_received_time(self) -> float | None:
+        return self._last_received_time
 
     def _on_receive(self, new: ComsMessage) -> Any:
         ...
